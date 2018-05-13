865bd7204bb32470e06852aa6f107eda4cc34058
Lairebaux <32078663+Lairebaux@users.noreply.github.com>
2018-05-13 10:35:48 -0400

    Add files via upload

---
+++ Time_Conversion/test_my_Time_Conversion.py
@@ -0,0 +1,16 @@
+import pytest
+from my_Time_Conversion import time_conversion
+
+
+@pytest.mark.parametrize("AM_PM, military",[
+    ("12:00:00AM", "00:00:00"),
+    ("02:15:36AM", "02:15:36"),
+    ("10:51:11AM", "10:51:11"),
+    ("12:00:00PM", "12:00:00"),
+    ("01:48:03PM", "13:48:03"),
+    ("11:05:45PM", "23:05:45"),
+    ])
+def test_time_conversion(AM_PM, military):
+    assert time_conversion(AM_PM) == military
+
+
